38fcf6bfc0c2f48d0633d5d2ffdbac3545262da1
Kostiantyn Nazarenko <const@nazarenko.es>
2026-09-08 04:44:14 +0200

    add button color states

--- src/ui/Button/index.tsx
+++ src/ui/Button/index.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import type { CSSProperties, FC, HTMLAttributes, JSX, MouseEvent, PropsWithChildren } from 'react';
-import Text from './Text';
 import './styles.scss';
 
 interface Props extends HTMLAttributes<HTMLButtonElement> {
@@ -11,7 +10,7 @@ interface Props extends HTMLAttributes<HTMLButtonElement> {
   color?: string;
 }
 
-const Button: FC<PropsWithChildren<Props>> = ({ color, onClick, children, className, disabled, ...rest }) => {
+const Button: FC<PropsWithChildren<Props>> = ({ color, icon, onClick, children, className, disabled, ...rest }) => {
   const style = {
     '--btn-color': color,
   } as CSSProperties;
@@ -24,15 +23,38 @@ const Button: FC<PropsWithChildren<Props>> = ({ color, onClick, children, classN
       disabled={disabled}
       style={style}
     >
+      {icon}
       {children}
     </button>
   );
 };
 
 interface ICompound extends FC<PropsWithChildren<Props>> {
-  Text: typeof Text;
+  Text: typeof Button;
+  Error: typeof Button;
+  Warning: typeof Button;
+  Info: typeof Button;
+  Primary: typeof Button;
+  Secondary: typeof Button;
+  Tertiary: typeof Button;
+  Legendary: typeof Button;
+  Epic: typeof Button;
 }
 
-(Button as ICompound).Text = Text;
+const variantCreator = (variantClass: string) => {
+  return ({ className, ...rest }: PropsWithChildren<Props>) => (
+    <Button {...rest} className={classNames(variantClass, className)} />
+  );
+};
+
+(Button as ICompound).Text = variantCreator('button-text');
+(Button as ICompound).Error = variantCreator('button-error');
+(Button as ICompound).Warning = variantCreator('button-warning');
+(Button as ICompound).Info = variantCreator('button-info');
+(Button as ICompound).Primary = variantCreator('button-primary');
+(Button as ICompound).Secondary = variantCreator('button-secondary');
+(Button as ICompound).Tertiary = variantCreator('button-tertiary');
+(Button as ICompound).Legendary = variantCreator('button-legendary');
+(Button as ICompound).Epic = variantCreator('button-epic');
 
 export default Button as ICompound;
